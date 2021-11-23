97b8a625954f0d7f458340a03e2dd5188e861f9d
edolaku <edoooq@gmail.com>
2021-11-24 06:40:14 +0700

    update materi nodejs

--- wpu/4-npm/app.js
+++ wpu/4-npm/app.js
@@ -8,36 +8,36 @@ const yargs = require("yargs");
 const contacts = require("./contacts");
 
 yargs.command({
-        // masukkan perintah
-        command: "tambah",
-        // deskripsi dari perintah
-        describe: "Menambahkan kontak baru",
-        //masukkan buildernya. isinya adalah objek, dimana kita menuliskan item2 yg kita butuhkan, terdapat option2nya
-        builder: {
-            nama: {
-                describe: "Nama lengkap",
-                // apakah ini wajib diisi? jika ya, masukkan true. jika tidak, masukkan false
-                demandOption: true,
-                type: "string",
-            },
-            email: {
-                describe: "Alamat email",
-                // apakah ini wajib diisi? jika ya, masukkan true. jika tidak, masukkan false
-                demandOption: false,
-                type: "string",
-            },
-            noHP: {
-                describe: "Nomor handphone",
-                // apakah ini wajib diisi? jika ya, masukkan true. jika tidak, masukkan false
-                demandOption: true,
-                type: "string",
-            }
+    // masukkan perintah
+    command: "tambah",
+    // deskripsi dari perintah
+    describe: "Menambahkan kontak baru",
+    //masukkan buildernya. isinya adalah objek, dimana kita menuliskan item2 yg kita butuhkan, terdapat option2nya
+    builder: {
+        nama: {
+            describe: "Nama lengkap",
+            // apakah ini wajib diisi? jika ya, masukkan true. jika tidak, masukkan false
+            demandOption: true,
+            type: "string",
         },
-        // handler => bentuknya function, akan menerima argumen, sehingga kita dapat menerima argumen
-        handler: (argv) => {
-            contacts.simpanKontak(argv.nama, argv.email, argv.noHP)
+        email: {
+            describe: "Alamat email",
+            // apakah ini wajib diisi? jika ya, masukkan true. jika tidak, masukkan false
+            demandOption: false,
+            type: "string",
         },
-    })
+        noHP: {
+            describe: "Nomor handphone",
+            // apakah ini wajib diisi? jika ya, masukkan true. jika tidak, masukkan false
+            demandOption: true,
+            type: "string",
+        }
+    },
+    // handler => bentuknya function, akan menerima argumen, sehingga kita dapat menerima argumen
+    handler: (argv) => {
+        contacts.simpanKontak(argv.nama, argv.email, argv.noHP)
+    },
+})
     // agar ketika masukkan node app pada terminal, ada fungsi yg dijalankan
     .demandCommand();
 
@@ -70,6 +70,18 @@ yargs.command({
     // handler => bentuknya function, 
     handler(argv) {
         contacts.detailKontak(argv.nama);
+    },
+})
+
+// menghapus kontak berdasarkan nama
+yargs.command({
+    // masukkan perintah
+    command: "hapus",
+    // deskripsi dari perintah
+    describe: "menghapus kontak berdasarkan nama",
+    // handler => bentuknya function, 
+    handler(argv) {
+        contacts.hapusKontak(argv.nama);
     },
 })
 
--- wpu/4-npm/contacts.js
+++ wpu/4-npm/contacts.js
@@ -28,7 +28,7 @@ const simpanKontak = (nama, email, noHP) => {
 
     const contacts = ambilKontak();
 
-    // cari duplikat pd contacts.json, jika nama yg diinput sudah pernah diinput, maka hasil dari const duplikat adalah true, dan jika tidak ada, hasilnya adalah undefined
+    // cari apakah kontak yg diinput sudah ada di database. ketika menggunakan .find ketika array yg dicari sudah ketemu, maka penelusuran akan berhenti, namun jika menggunakan .filter, dia akan menelusuri hingga selesai ke semua array. cari duplikat pd contacts.json, jika nama yg diinput sudah pernah diinput, maka hasil dari const duplikat adalah true, dan jika tidak ada, hasilnya adalah undefined
     const duplikat = contacts.find((contact) => contact.nama === nama);
     if (duplikat) {
         console.log("Nama sudah terdaftar!");
@@ -68,16 +68,32 @@ const detailKontak = (nama) => {
 
     const contacts = ambilKontak();
 
-    // cari apakah kontak yg diinput sudah ada di database
-    const contact = contacts.find((contact) => { contact.nama === nama });
+    // cari apakah kontak yg diinput sudah ada di database. ketika menggunakan .find ketika array yg dicari sudah ketemu, maka penelusuran akan berhenti, namun jika menggunakan .filter, dia akan menelusuri hingga selesai ke semua array
+    const contact = contacts.find((contact) => contact.nama.toLowerCase === nama.toLowerCase);
 
+    // jika nama tidak ada, tampilkan ${nama} tidak ditemukan
     if (!contact) {
         console.log(`${nama} tidak ditemukan`);
         return false;
     };
     console.log(contact.nama);
+    if (contact.email) {
+        console.log(contact.email);
+    }
+    console.log(contact.noHP);
+}
 
+const hapusKontak = (nama) => {
+    // cara menghapus disini adalah dengan cara mencari nama yg diinput, lalu membuat array baru, dan mengcopy semua array yg ada di database, lalu akan dicopykan lagi ke database, tetapi tanpa mengikutkan nama yg diinputkan tadi, sehingga nama tersebut akan hilang dari database
+    const contacts = ambilKontak();
+    const contact = contacts.find((contact) => contact.nama.toLowerCase === nama.toLowerCase);
+    const newContact = contact.splice(0, 2);
+    fs.writeFileSync(filePath, JSON.stringify(newContact, null, 2));
+    // cari apakah kontak yg diinput sudah ada di database. ketika menggunakan .find ketika array yg dicari sudah ketemu, maka penelusuran akan berhenti, namun jika menggunakan .filter, dia akan menelusuri hingga selesai ke semua array
+    // jadi .filter dibawah ini adalah mengkopy kontak yg ada pd database (contacts), tapi dengan TANPA nama yg diinputkan
+    // const newContact = contacts.filter((contact) => contact.nama.toLowerCase !== nama.toLowerCase);
+    // fs.writeFileSync(filePath, JSON.stringify(newContact, null, 2));
 }
 
 // export fungsi simpanKontak dan tulisPertanyaan ke app.js, karena pd objek, key dan valuenya sama jadi tulis satu saja
-module.exports = { simpanKontak, daftarKontak, detailKontak };
+module.exports = { simpanKontak, daftarKontak, detailKontak, hapusKontak };
